54294d8c11accdb5fcb726dd4f77b5555160a969
Iwona Antonik <iwona.antonik@gmail.com>
2019-02-23 10:50:25 -0500

    html tags

---
+++ app.js
@@ -0,0 +1,14 @@
+
+//on click to push data to table
+
+var name;
+var 
+
+
+$( "#" ).on( "click", function() {
+
+    $("#btn2").click(function(){
+        $("#employeeName").html(`<td>${name}</td>`);
+      });
+    
+  });
